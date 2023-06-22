b4be9c6a000285bceaaade2f2d2afa0ab6f7710a
Jeremy Thurston <jeremy.thurston@colorado.edu>
2023-06-22 14:25:46 -0500

    Bug fixes

--- import_oceanoptics_data.py
+++ import_oceanoptics_data.py
@@ -3,13 +3,13 @@ import numpy as np
 
 
 def import_oceanoptics_data(filename):
-    data = pd.read_csv(filename, skiprows=13, delimiter="\t", names=["wls", "counts"])
+    data = pd.read_csv(filename, delimiter=",", names=["wls", "counts"])
     wls, counts = data["wls"], data["counts"]
-    baseline = np.median(np.sort(counts)[counts < (0.05 * np.max(counts))])
-    counts = counts - baseline
+    # baseline = np.median(np.sort(counts)[counts < (0.05 * np.max(counts))])
+    # counts = counts - baseline
     counts = counts / np.max(counts)
 
-    wls = wls.to_numpy()
-    counts = counts.to_numpy()
+    wls = wls.to_numpy(dtype=float)
+    counts = counts.to_numpy(dtype=float)
 
     return [wls, counts]
--- main.py
+++ main.py
@@ -4,64 +4,86 @@
 # Date: June 20, 2023
 # --------------------------------------------------------------
 
-# Imports ------------------------------------------------------
-import import_frog_data
-import import_oceanoptics_data
+# ----------------------- Imports ------------------------------
+from import_frog_data import import_frog_data
+from import_oceanoptics_data import import_oceanoptics_data
 import matplotlib.pyplot as plt
+import numpy as np
+
 # --------------------------------------------------------------
 
 
-# Make the plots pretty ----------------------------------------
-plt.style.use(["science", "nature", "bright"])
+# ---------------- Make the plots pretty -----------------------
+plt.style.use(["science", "nature", "bright", "no-latex"])
 plt.rcParams["font.family"] = "Helvetica"
 plt.rcParams["font.size"] = 11
 c1, c2 = "#33BBEE", "#EE7733"
 # --------------------------------------------------------------
 
-# Parameters ---------------------------------------------------
-frog_folder = "FROG retrievals/"
-spectrum_file = "OceanOptics Spectra/"
-wl_minimum = 700  # Minimum wavelength [nm]
-wl_maximum = 900  # Maximum wavelength [nm]
-t_minimum = -200  # Minimum time [fs]
-t_maximum = 200  # Maximum time [fs]
+# ---------------------- Parameters ----------------------------
+frog_folder = "FROG retrievals/FROG8"
+spectrum_file = "OceanOptics Spectra/FROG8_spectrum.txt"
+wl_minimum = 1400  # Minimum wavelength [nm]
+wl_maximum = 1700  # Maximum wavelength [nm]
+t_minimum = -500  # Minimum time [fs]
+t_maximum = 500  # Maximum time [fs]
+FROG_threshold = 0.03  # Phase data below this threshold is not shown
 # --------------------------------------------------------------
 
 
 if __name__ == "__main__":
     # Import data from FROG retrieval
     FROG_data = import_frog_data(frog_folder)
+    ROI = np.where(FROG_data[1] > FROG_threshold)
 
     # Import data from OceanOptics spectrometer
     Spectrometer_data = import_oceanoptics_data(spectrum_file)
 
     # Process data
+    # Calculate TL
 
     # Generate plot
     fig, axes = plt.subplots(2, 1, figsize=(3, 3))
 
-    axes[0].plot(wls_frog, spectral_intensity, color=c1, label="Retrieved")
-    axes[0].set_ylabel("Intensity (arb)")
-    axes[0].set_xlabel("Wavelength (nm)")
-    axes[0].set_xlim(950, 1100)
-
-    axes[1].plot(
-        t_TL_frog,
-        E_TL_frog,
-        label=f"TL of FROG spectrum, {fwhm_TL_frog:.1f} fs",
+    # Generate spectrum figure
+    ax2 = axes[0].twinx()
+    axes[0].plot(
+        FROG_data[0], FROG_data[1], color=c1, label="Retrieved", linewidth=0.75
+    )
+    axes[0].fill_between(
+        Spectrometer_data[0],
+        0,
+        Spectrometer_data[1],
         color=c1,
+        label="Measured",
+        alpha=0.3,
+        linestyle="--",
     )
-    axes[1].plot(
-        ts_frog - ts_frog[np.argmax(intensity_t)],
-        intensity_t,
-        label=f"Retrieved pulse, {fwhm_retrieved:.1f} fs",
-        color=c4,
-    )
-    axes[1].set_xlabel("Time (fs)")
-    axes[1].set_ylabel("Intensity (arb)")
-    axes[1].set_xlim(-5.5 * fwhm_TL_direct, 10.5 * fwhm_TL_direct)
-    axes[1].legend()
+    ax2.plot(FROG_data[0][ROI], FROG_data[2][ROI], color=c2, linewidth=0.75)
+    axes[0].set_ylabel("Intensity (arb)")
+    ax2.set_ylabel("Phase (rad)")
+    axes[0].set_xlabel("Wavelength (nm)")
+    axes[0].set_xlim(wl_minimum, wl_maximum)
+    axes[0].legend()
+
+    # Generate temporal figure
+    # axes[1].plot(
+    #     t_TL_frog,
+    #     E_TL_frog,
+    #     label=f"TL of FROG spectrum, {fwhm_TL_frog:.1f} fs",
+    #     color=c1,
+    # )
+    # axes[1].plot(
+    #     ts_frog - ts_frog[np.argmax(intensity_t)],
+    #     intensity_t,
+    #     label=f"Retrieved pulse, {fwhm_retrieved:.1f} fs",
+    #     color=c4,
+    # )
+    # axes[1].set_xlabel("Time (fs)")
+    # axes[1].set_ylabel("Intensity (arb)")
+    # axes[1].set_xlim(t_minimum, t_maximum)
+    # axes[1].legend()
 
     fig.tight_layout()
-    plt.savefig("figures/frog/combined_frog_plot.jpg", dpi=500)
+    plt.savefig("figures/combined_frog_plot.jpg", dpi=500)
     plt.show()
